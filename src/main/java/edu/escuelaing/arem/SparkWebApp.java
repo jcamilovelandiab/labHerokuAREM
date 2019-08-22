aa7859f35424da5071f0628779de7fe655f8a4b1
jcamilovelandiab <jcamilovelandiab@gmail.com>
2019-08-22 18:41:11 -0500

    no sirve el Procfile

--- src/main/java/edu/escuelaing/arem/SparkWebApp.java
+++ src/main/java/edu/escuelaing/arem/SparkWebApp.java
@@ -7,17 +7,16 @@ import static spark.Spark.*;
  */
 public class SparkWebApp 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        port(getPort());
         get("/hello", (req, res) -> "Hello Heroku");
     }
-
+    
     static int getPort() {
         if (System.getenv("PORT") != null) {
-            return Integer.parseInt(System.getenv("PORT"));
+        return Integer.parseInt(System.getenv("PORT"));
         }
-        return 4567; //returns default port if heroku-port isn't set (i.e. on localhost)
+        return 4567; //returns default port if heroku-port isn't set(i.e. on localhost)
     }
 
 }
