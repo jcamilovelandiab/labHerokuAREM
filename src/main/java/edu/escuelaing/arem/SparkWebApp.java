6a98869c0b3a56742e06a0a791544434713c4630
jcamilovelandiab <jcamilovelandiab@gmail.com>
2019-08-22 17:15:35 -0500

    creando clases spark

--- src/main/java/edu/escuelaing/arem/SparkWebApp.java
+++ src/main/java/edu/escuelaing/arem/SparkWebApp.java
@@ -1,13 +1,15 @@
 package edu.escuelaing.arem;
+import static spark.Spark.*;
 
 /**
  * Hello world!
  *
  */
-public class App 
+public class SparkWebApp 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        get("/hello", (req, res) -> "Hello Heroku");
     }
 }
